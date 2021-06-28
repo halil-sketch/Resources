ccb9f3829e381228666a4ebb957da8320c3fcd98
HalilyDev <81930992+haliliyy@users.noreply.github.com>
2021-06-28 19:02:03 +0300

    Add files via upload

---
+++ hy_troll/troll.lua
@@ -0,0 +1,24 @@
+addEventHandler("onPlayerCommand", root,function(sex)
+    if sex == "murat" then
+        cancelEvent()
+        outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox("[Admin Warn]#f2f2f2Murat_Sarsilmaz adli kullanicinin ranki sistem tarafindan Sunucu Sorumlusuna yükseltildi",source,255,0,0,true)
+		outputChatBox(" ",source,255,0,0,true)
+		outputChatBox("[Admin Warn]#f2f2f2Onaylamak için /yetkionay yaz.",source,255,0,0,true)
+        
+    end
+end)
+
+addEventHandler("onPlayerCommand", root,function(komut)
+    if komut == "yetkionay" then
+        cancelEvent()
+        exports["orp_infobox"]:addBox(source,"success","Haliliyy'den selamlar sayın Murat_Sarsilmaz.")    
+		exports["orp_infobox"]:addBox(source,"success","SİKTİR GİT APTAL OROSPU ASHJFGASKFHGAKJFGAJHFGJAHGF")    
+    end
+end)
