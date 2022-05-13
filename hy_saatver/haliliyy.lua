57a5c5366f64017cb650290bd8941a1e3b8ed86c
ASDJASHFKJHASHAJKS <81930992+halil-sketch@users.noreply.github.com>
2022-05-13 22:54:35 +0300

    Update haliliyy.lua

--- hy_saatver/haliliyy.lua
+++ hy_saatver/haliliyy.lua
@@ -1,16 +1,62 @@
-﻿function saat_ver(thePlayer, cmd, verilen_saat)
-    if not verilensaat then
-        outputChatBox("#cc0000[!] #FFFFFFKomut eksik yada yanlış.", thePlayer, 0, 255, 0, true)
-        return
-    end
-    if getElementData(thePlayer, "account:username") == "halil" or getElementData(thePlayer, "account:username") == "haliliyy" then
-        for , player in ipairs(exports.pool:getPoolElementsByType("player")) do
-            local saat_cek = getElementData(player, "hoursplayed") or 1
-            local yeni_saat = saat_cek + tonumber(verilen_saat)
-            exports['anticheat-system']:changeProtectedElementDataEx(player, "hoursplayed", yeni_saat, false, true)
-            mysql:query_free("UPDATE characters SET hoursplayed= hoursplayed +".. tonumber(verilen_saat) .." WHERE id ='" .. mysql:escape_string(tostring(getElementData( player, "dbid" ))) .. "'" )
-            outputChatBox("[!] #FFFFFFTebrikler, ".. verilen_saat .." saat bonusunu aldınız.", player, 0, 255, 0, true)
-        end
+giveHour = function(p,cmd,t,m) -- player, cmd, target, miktar
+
+    if not t and not tonumber(m) then
+        p:outputChat("SYNTAX:#FFFFFF /"..cmd.." [Oyuncu] [Verilecek Saat]",155,155,155,true)
+    return end
+
+    local targetPlayer, targetPlayerName = exports.wrp_global:findPlayerByPartialNick(p, t)
+
+    local dbid = targetPlayer:getData("dbid")
+    local hoursplayed = targetPlayer:getData("hoursplayed") or 0
+    targetPlayer:setData("hoursplayed",hoursplayed+m)
+    exports['wrp_anticheat']:changeProtectedElementDataEx(targetPlayer, "hoursplayed", hoursplayed+m, false, true)
+    dbExec(getDBConnection(), "UPDATE characters SET hoursplayed = hoursplayed + '"..m.."' WHERE id = '" .. dbid.."' ")
+    targetPlayer:outputChat("Sunucu:#FFFFFF Hesabınıza "..m.." saat eklendi.",155,155,155,true)
+    p:outputChat("Sunucu:#FFFFFF Kişinin hesabına "..m.." saat eklendi.",155,155,155,true)
+
+
+end
+addCommandHandler("saatver",giveHour)
+
+
+
+
+
+
+
+
+
+function connect(res)
+    DBConnection = dbConnect( "mysql", "dbname="..dbName..";host="..hostName..";charset=utf8", ""..kadi.."", ""..sifre.."" )
+    if (not DBConnection) then
+        outputDebugString("Error: Failed to establish connection to the MySQL database server")
+    else
+        outputDebugString(""..getResourceName(res)..": Baglanti Saglandi ")
+        outputDebugString(""..getResourceName(res)..": Host: "..hostName.."")
+        outputDebugString(""..getResourceName(res)..": Database: "..dbName.."")
+        outputDebugString(""..getResourceName(res)..": Kullanici: "..kadi.."")
+        outputDebugString(""..getResourceName(res)..": Sifre: "..sifre.."")
     end
 end
-addCommandHandler("saatver", saat_ver)
+
+addEventHandler("onResourceStart",resourceRoot, connect)
+ 
+function query(...)
+    local queryHandle = dbQuery(DBConnection, ...)
+    if (not queryHandle) then
+        return nil
+    end
+    local rows = dbPoll(queryHandle, -1)
+    return rows
+end
+ 
+function execute(...)
+    local queryHandle = dbQuery(DBConnection, ...)
+    local result, numRows = dbPoll(queryHandle, -1)
+    return numRows
+end
+
+function getDBConnection()
+    return DBConnection
+end
+
